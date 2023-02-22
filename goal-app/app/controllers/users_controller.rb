b569bf7c33c053b069a6d1cd20f11c30d31ab4f9
abaez8031 <abaez8031@gmail.com>
2023-02-22 16:23:43 -0500

    hey sexy

---
+++ goal-app/app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def new
+    render :new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
